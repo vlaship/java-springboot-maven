9c1199d141de3ad079a28258f425858a3fae2bf3
vlaship <vlaship@outlook.com>
2025-02-11 22:48:07 -0500

    value

--- book-store-facade/src/main/java/com/book/store/facade/config/FacadeConfig.java
+++ book-store-facade/src/main/java/com/book/store/facade/config/FacadeConfig.java
@@ -1,13 +1,30 @@
 package com.book.store.facade.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
+import java.time.Duration;
+
 @Configuration
 public class FacadeConfig {
+
+    @Value("${rest.connect-timeout:10}")
+    private int connectTimeout;
+    @Value("${rest.read-timeout:25}")
+    private int readTimeout;
+
+    @Autowired
+    private RestTemplateBuilder restTemplateBuilder;
+
     @Bean
     public RestTemplate restTemplate() {
-        return new RestTemplate();
+        return restTemplateBuilder
+                .setConnectTimeout(Duration.ofSeconds(connectTimeout))
+                .setReadTimeout(Duration.ofSeconds(readTimeout))
+                .build();
     }
 }
